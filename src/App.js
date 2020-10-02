88b1bd6289181bbcacd244eadbcec508f2df7851
LeeWannacott <wannacottL@gmail.com>
2020-10-02 23:28:00 +1300

    Enable search for users

--- src/App.js
+++ src/App.js
@@ -11,23 +11,31 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    this.setState({ Loading: true });
+  // async componentDidMount() {
+  //   this.setState({ Loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+
+  //   console.log(res.data);
+  // }
+
+  searchUsers = async (text) => {
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
-    this.setState({ users: res.data, loading: false });
-
-    console.log(res.data);
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="GitHub finder" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,11 +6,11 @@ export class Search extends Component {
   };
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     return (
       <div>
