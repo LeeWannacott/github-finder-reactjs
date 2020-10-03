73571a320a36c48918078de4048986203bc22c17
LeeWannacott <wannacottL@gmail.com>
2020-10-03 23:20:23 +1300

    Added react routing

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>About this App</h1>
+        <p>App to search Gitbhub users</p>
+        <p>Version 1.0.0</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default About;
